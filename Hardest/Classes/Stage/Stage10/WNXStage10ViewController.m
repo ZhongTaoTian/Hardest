4d4137f96f0cc5b65ba4c0b2a00cca9856ff5bd1
田中涛 <macbook@tianzhongtao.local>
2016-04-23 14:17:10 +0800

    Stage10 infoPlist modify

--- Hardest/Classes/Stage/Stage10/WNXStage10ViewController.m
+++ Hardest/Classes/Stage/Stage10/WNXStage10ViewController.m
@@ -16,22 +16,17 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+
+    [self buildStageInfo];
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (void)buildStageInfo {
+    UIImageView *bgIV = [[UIImageView alloc] initWithFrame:ScreenBounds];
+    bgIV.image = [UIImage imageNamed:@"08_bg-iphone4"];
+    [self.view insertSubview:bgIV belowSubview:self.redButton];
+    
+    [super bringPauseAndPlayAgainToFront];
 }
 
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
 
 @end
